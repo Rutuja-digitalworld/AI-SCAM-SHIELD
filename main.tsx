b449f577c4889e3d52d372eba54b6c37be2c916e
Rutuja Kadam <rutujakadam536@gmail.com>
2026-09-16 15:50:12 +0530

    Fix import path for ThemeProvider component

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { ThemeProvider } from './lib/theme.tsx';
+import { ThemeProvider } from './theme.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
